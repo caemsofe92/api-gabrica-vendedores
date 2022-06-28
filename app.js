ba6117f3d44aebe069799d8bef4de50fec37a82f
Carlos Soto <carlos.soto@srfconsultores.com>
2022-06-28 16:48:11 -0500

    Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+const compression = require("compression");
+const dotenv = require('dotenv');
+dotenv.config();
+
+var indexRouter = require("./routes/index");
+var getHome = require("./routes/get-home");
+
+
+var app = express();
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
+app.use("/", indexRouter);
+app.use("/get-home", getHome);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
