9b3d6e7a1d86097fb7942c42985782ea16681aa0
jaime-rodriguez-sarmiento <jaimestebanrodriguez@gmail.com>
2022-06-30 15:42:34 -0500

    commit test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Gabrica Vendedores API Training");
+  res.send("Gabrica Vendedores API Training_");
 });
 
 module.exports = router;
