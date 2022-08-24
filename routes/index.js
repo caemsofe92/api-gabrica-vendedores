9a5d9f8930373d341bbb936a6d96b2562ebc62f0
caemsofe92 <carlosemsofe@gmail.com>
2022-08-24 00:04:05 -0500

    Commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Gabrica Vendedores API");
+  res.send("Gabrica Vendedores API Version 1.0");
 });
 
 module.exports = router;
